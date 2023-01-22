7071a350b66b6e4af459c1d761e01d3039790918
a.v.verkhoglyad <a.v.verkhoglyad@gmail.com>
2023-01-23 00:22:30 +0300

    gateway server

---
+++ gateway-server/src/main/java/io/averkhoglyad/ostock/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,15 @@
+package io.averkhoglyad.ostock.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+}
