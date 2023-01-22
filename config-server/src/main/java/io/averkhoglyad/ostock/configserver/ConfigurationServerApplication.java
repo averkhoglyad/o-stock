d698c35b89703825ea1d5303faf858820d1010e5
a.v.verkhoglyad <a.v.verkhoglyad@gmail.com>
2023-01-23 00:14:29 +0300

    config server, fix package name

--- config-server/src/main/java/io/averkhoglyad/ostock/configserver/ConfigurationServerApplication.java
+++ config-server/src/main/java/io/averkhoglyad/ostock/configserver/ConfigurationServerApplication.java
@@ -1,4 +1,4 @@
-package io.verkhoglyad.configserver;
+package io.averkhoglyad.ostock.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
