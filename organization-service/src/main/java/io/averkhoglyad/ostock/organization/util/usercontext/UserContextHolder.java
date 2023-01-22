6336b4d69a5b579845a6fa7dd2744b435e027aa4
a.v.verkhoglyad <a.v.verkhoglyad@gmail.com>
2023-01-23 01:16:06 +0300

    organization service, add tracking id and user context support

---
+++ organization-service/src/main/java/io/averkhoglyad/ostock/organization/util/usercontext/UserContextHolder.java
@@ -0,0 +1,28 @@
+package io.averkhoglyad.ostock.organization.util.usercontext;
+
+import org.springframework.util.Assert;
+
+public class UserContextHolder {
+
+    private static final ThreadLocal<UserContext> userContext = new ThreadLocal<>();
+
+    public static final UserContext getContext() {
+        UserContext context = userContext.get();
+
+        if (context == null) {
+            context = createEmptyContext();
+            userContext.set(context);
+
+        }
+        return userContext.get();
+    }
+
+    public static final void setContext(UserContext context) {
+        Assert.notNull(context, "Only non-null UserContext instances are permitted");
+        userContext.set(context);
+    }
+
+    public static final UserContext createEmptyContext() {
+        return new UserContext();
+    }
+}
