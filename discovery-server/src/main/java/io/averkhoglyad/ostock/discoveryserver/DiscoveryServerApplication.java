0ae20363790b16167c7c0dbc60c63818113ef807
a.v.verkhoglyad <a.v.verkhoglyad@gmail.com>
2023-01-23 00:09:32 +0300

    discovery server, fix package name

--- discovery-server/src/main/java/io/averkhoglyad/ostock/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/io/averkhoglyad/ostock/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package io.verkhoglyad.dispatcherserver;
+package io.averkhoglyad.ostock.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
